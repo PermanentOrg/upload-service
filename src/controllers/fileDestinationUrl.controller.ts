80b3999a39a25127ce66b9eea5febbeaca26b0ea
Dan Schultz <slifty@gmail.com>
2020-10-22 14:49:35 -0400

    Add validation
    This adds validation to the fileDestinationUrl api endpoints and creates
    a consistent error serialization format for internal errors as well as
    validation errors.
    
    Issue #7

--- src/controllers/fileDestinationUrl.controller.ts
+++ src/controllers/fileDestinationUrl.controller.ts
@@ -1,23 +1,27 @@
 import type { Request, Response } from 'express';
+import type { CreateFileDestinationUrlParams } from '../services/fileDestinationUrl.service';
 import { fileDestinationUrlService } from '../services';
+import { validateCreateFileDestinationUrlParams } from '../validators';
+import { serializeError } from '../utils';
 
 interface CreateFileDestinationRequest extends Request {
-  body: {
-    bucket: string;
-    fileName: string;
-    fileType: string;
-    maxSize: number;
-    path: string;
-  };
+  body: CreateFileDestinationUrlParams;
 }
 
 const createFileDestinationUrl = (
   req: CreateFileDestinationRequest,
   res: Response,
 ): void => {
-  fileDestinationUrlService.createFileDestinationUrl(req.body)
-    .then((data) => res.json(data))
-    .catch((err) => res.status(500).json(err));
+  const validation = validateCreateFileDestinationUrlParams(req.body);
+  if (validation.error) {
+    res.status(400).json({ error: validation.error });
+  } else {
+    fileDestinationUrlService.createFileDestinationUrl(req.body)
+      .then((data) => res.json(data))
+      .catch((err) => res.status(500).json({
+        error: serializeError(err),
+      }));
+  }
 };
 
 export const fileDestinationUrlController = {
