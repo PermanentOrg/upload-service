f30150c816cf5150e9dd3bc281a1c326c7cef651
Dan Schultz <slifty@gmail.com>
2020-10-22 14:26:24 -0400

    Refactor app definition
    By putting the express app definition in its own file it becomes
    possible to use the same app for integration tests, since it can be
    imported not just by the service logic but also by various endpoint
    tests.
    
    Issue #7

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { apiRoutes } from './routes';
+import { app } from './app';
 
-const app = express();
-app.use(bodyParser.json());
-app.use('/api', apiRoutes);
 app.listen(process.env.PORT ?? 3000);
