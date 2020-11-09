f4f8d4a35ee03b1df2bc44923ca013021d5a3410
Dan Schultz <slifty@gmail.com>
2020-11-09 09:30:06 -0500

    Add Sentry
    This allows Sentry to be set up when running or deploying this project.
    
    Issue #3

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+import * as Sentry from '@sentry/node';
 import { app } from './app';
+
+if ('SENTRY_DSN' in process.env
+ && 'SENTRY_ENVIRONMENT' in process.env) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    tracesSampleRate: 1,
+    environment: process.env.SENTRY_ENVIRONMENT,
+  });
+}
 
 app.listen(process.env.PORT ?? 3000);
