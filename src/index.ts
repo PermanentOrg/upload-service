a1db573f83f31a0c007e4d13fb545a306261b678
Liam Lloyd <wlloyd@hmc.edu>
2024-05-28 12:23:47 -0700

    Move Sentry initialization to instrument.js
    In their v8 release, Sentry added automatic instrumentation for a
    variety of node packages. For this to work, Sentry must be initialized
    before any other import. To achieve this, this commit moves Sentry
    initialization into an instrument.js file, which is now the first import in
    index.js, as recommended in the Sentry release notes.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-import * as Sentry from '@sentry/node';
+import "./instrument";
 import { app } from './app';
 import { logger } from './log';
-
-if ('SENTRY_DSN' in process.env
- && 'SENTRY_ENVIRONMENT' in process.env) {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    tracesSampleRate: 1,
-    environment: process.env.SENTRY_ENVIRONMENT,
-  });
-}
 
 const port = process.env.PORT ?? 3000;
 app.listen(port, () => {
