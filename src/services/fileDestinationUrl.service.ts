60296acdf5f558fee7c511c6d4a9ba80e101b323
Dan Schultz <slifty@gmail.com>
2025-07-03 10:18:43 -0400

    Enable `@typescript-eslint/consistent-type-imports`
    Issue #587 Re-enable love lint rules

--- src/services/fileDestinationUrl.service.ts
+++ src/services/fileDestinationUrl.service.ts
@@ -3,7 +3,7 @@ import {
 	CreateMultipartUploadCommand,
 	UploadPartCommand,
 	CompleteMultipartUploadCommand,
-	CompletedPart,
+	type CompletedPart,
 } from "@aws-sdk/client-s3";
 import { createPresignedPost } from "@aws-sdk/s3-presigned-post";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
